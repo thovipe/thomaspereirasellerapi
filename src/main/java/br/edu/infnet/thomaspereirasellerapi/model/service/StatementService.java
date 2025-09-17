09b140916a469ca9068d4b42bb6da12461654ecc
Thomas <thovipe@gmail.com>
2025-09-17 20:42:36 -0300

    Sexto ciclo - Green. Testes efetuados com sucesso.

--- src/main/java/br/edu/infnet/thomaspereirasellerapi/model/service/StatementService.java
+++ src/main/java/br/edu/infnet/thomaspereirasellerapi/model/service/StatementService.java
@@ -1,11 +1,14 @@
 package br.edu.infnet.thomaspereirasellerapi.model.service;
 
+import br.edu.infnet.thomaspereirasellerapi.model.domain.StamentStatus;
 import br.edu.infnet.thomaspereirasellerapi.model.domain.Statement;
 import br.edu.infnet.thomaspereirasellerapi.model.exception.InvalidItemValueException;
 import br.edu.infnet.thomaspereirasellerapi.model.exception.InvalidMonthReferenceException;
 import br.edu.infnet.thomaspereirasellerapi.model.exception.InvalidQuantityItemValueException;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.security.InvalidParameterException;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class StatementService {
@@ -49,9 +52,27 @@ public class StatementService {
        return test.get();
     }
 
-    // TODO Create a method for applying discounts it calls monthlyStatementCalculation and applies a discount received as parameter
     public BigDecimal monthlyStatementWithDiscountCalculation(Statement statement, Integer discount) {
-        return null;
+
+        BigDecimal finalStatementValue = BigDecimal.ZERO;
+
+        if(statement.getStatus() == StamentStatus.OVERDUE) {
+            return finalStatementValue;
+        }
+        if( discount == null || discount <= 0 ) {
+            throw new InvalidParameterException("Discount cannot be null or zero.");
+        }
+
+       if(discount <= 30)  {
+            BigDecimal statementWithoutDiscount = monthlyStatementCalculation(statement);
+            BigDecimal dicountValue = statementWithoutDiscount.multiply(BigDecimal.valueOf(discount));
+            finalStatementValue = dicountValue.divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP);
+            finalStatementValue = statementWithoutDiscount.subtract(finalStatementValue);
+            return finalStatementValue;
+        } else {
+            return monthlyStatementWithDiscountCalculation(statement, 30);
+        }
+
     }
 
 }
